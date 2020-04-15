19aa9b7a10862509809dd03eb78cb8f00930da19
Juan Luis Ramirez Garcia <juanlux.rg@gmail.com>
2020-04-15 15:37:14 +0200

    Add sidebar metrics for community and stacked chart

--- apps/app3.py
+++ apps/app3.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output, State
@@ -50,10 +51,10 @@ for i, (metric, value) in enumerate(dataset.metrics_dict.items()):
             dbc.CardBody(
                 [html.H6([metric],
                          className="card-subtitle", style={'marginBottom': '5px'}),
-                    html.H4("{:n}".format(
-                        value), className="card-title"),
-                    html.H6(["🠝", dataset.inc_dict[metric], " (24h.)"],
-                            className="card-subtitle"),
+                    html.H4("0", id="val_{}".format(
+                        metric), className="card-title"),
+                    html.H6(["🠝 0 (24h.)"],
+                            className="card-subtitle", id="inc_{}".format(metric)),
 
 
 
@@ -80,50 +81,25 @@ page = dbc.Container(
                 ),
                     html.Hr(className="my-2"),
                     dcc.Graph(
-                        id='daily-evolution',
-                        figure={
-                            'data': [
-                                {'x': pd.DatetimeIndex(dataset.df[dataset.df['CCAA'] == 'Andalucía']['Fecha']).to_pydatetime(),
-                                 'y': dataset.df[dataset.df['CCAA'] == 'Andalucía']['Casos'], 'type': 'scatter', 'name': 'Casos',  'marker': {'color': 'rgb(39, 128, 227)'}},
-                                {'x': pd.DatetimeIndex(dataset.df[dataset.df['CCAA'] == 'Andalucía']['Fecha']).to_pydatetime(),
-                                 'y': dataset.df[dataset.df['CCAA'] == 'Andalucía']['Fallecidos'], 'type': 'scatter', 'name': 'Fallecidos', 'marker': {'color': 'rgb(55, 58, 60)'}},
-                                {'x': pd.DatetimeIndex(dataset.df[dataset.df['CCAA'] == 'Andalucía']['Fecha']).to_pydatetime(),
-                                 'y': dataset.df[dataset.df['CCAA'] == 'Andalucía']['Recuperados'], 'type': 'scatter', 'name': 'Recuperados', 'marker': {'color': 'rgb(63, 182, 24)'}},
-                                {'x': [datetime.strptime('2020-03-14 00:00', '%Y-%m-%d %H:%M'), datetime.strptime('2020-03-14 00:01', '%Y-%m-%d %H:%M')], 'y': [
-                                    0, dataset.df[dataset.df['CCAA'] == 'Andalucía']['Casos'].max()], 'type': 'line', 'name': 'Estado de alarma', 'line': {'color': 'rgb(100, 105, 109)', 'width': '4', 'dash': 'dot'}},
-                                {'x': [datetime.strptime('2020-03-29 00:00', '%Y-%m-%d %H:%M'), datetime.strptime('2020-03-29 00:01', '%Y-%m-%d %H:%M')], 'y': [
-                                    0, dataset.df[dataset.df['CCAA'] == 'Andalucía']['Casos'].max()], 'type': 'line', 'name': 'Confinamiento estricto', 'line': {'color': 'rgb(143, 147, 150)', 'width': '4', 'dash': 'dot'}}
-                            ],
-                            'layout': go.Layout(
-                                paper_bgcolor='rgba(0,0,0,0)',
-                                plot_bgcolor='rgba(0,0,0,0)',
-                                legend=dict(orientation='h', y=1.2)
-                            )
-                        }
+                        id='daily-evolution', style={'marginTop': '15px', 'height': '280px'}
                 )], width={"size": 10, "offset": 2})
                 ],
-                style={"paddingLeft": "10px", 'height': '560px'}
+                style={"paddingLeft": "10px", 'paddingBottom': '20px'}
                 ),
 
         dbc.Row([
-            dbc.Col([html.H1("Evolución por comunidades", className="display-5", style={'paddingTop': '15px'}),
+            dbc.Col([html.H1("Incrementos diarios", className="display-5", style={'paddingTop': '15px'}),
                      html.P(
-                "Gráfico acumulado de casos por comunidad autónoma.",
+                "Cantidad diaria de cada tipo",
                 className="lead",
             ),
                 html.Hr(className="my-2"),
-                dcc.Graph(figure={
-                    'data': dataset.data,
-                    'layout': go.Layout(
-                        paper_bgcolor='rgba(0,0,0,0)',
-                        plot_bgcolor='rgba(0,0,0,0)',
-                        legend=dict(orientation='h', y=1.2),
-                        barmode='stack'
-                    )
-                }
+                dcc.Graph(
+                    id='daily-increment',
+                    style={'marginTop': '15px', 'height': '280px'}
             )], width={"size": 10, "offset": 2})
         ],
-            style={"paddingLeft": "10px", 'height': '560px'}
+            style={"paddingLeft": "10px", 'paddingBottom': '20px'}
         )
 
 
@@ -153,6 +129,135 @@ def update_daily_evolution(comunidad):
         'layout': go.Layout(
             paper_bgcolor='rgba(0,0,0,0)',
             plot_bgcolor='rgba(0,0,0,0)',
-            legend=dict(orientation='h', y=1.2)
+            legend=dict(orientation='h', y=1.2),
+            margin=dict(
+                l=0,
+                r=20,
+                b=25,
+                t=0),
+            xaxis={
+                'showgrid': False,
+                'autorange': True,
+                'zeroline': False,
+                'showline': False,
+
+
+            },
+            yaxis={
+                'showgrid': False,
+                'autorange': True,
+                'zeroline': False,
+                'showline': False,
+                'showticklabels': False
+
+            }
         )
     }
+
+
+@app.callback(
+    Output('daily-increment', 'figure'), [Input('comunidad-drop', 'value')]
+)
+def update_daily_increment(comunidad):
+    df = dataset.df[dataset.df['CCAA'] == comunidad]
+    data = []
+    cols = ['Casos', 'Fallecidos', 'Recuperados']
+
+    for col in cols:
+        data.append(
+            {
+                'x': pd.DatetimeIndex(df['Fecha']).to_pydatetime(),
+                'y': df[col].diff(),
+                'name': col,
+                'type': 'bar',
+                'text': df[col].diff(),
+                'textposition': 'auto'
+            }
+        )
+
+    return {
+        'data': data,
+        'layout': go.Layout(
+            paper_bgcolor='rgba(0,0,0,0)',
+            plot_bgcolor='rgba(0,0,0,0)',
+            legend=dict(orientation='h', y=1.2),
+            barmode='stack',
+            margin=dict(
+                l=0,
+                r=20,
+                b=25,
+                t=0),
+            xaxis={
+                'showgrid': False,
+                'autorange': True,
+                'zeroline': False,
+                'showline': False,
+
+
+            },
+            yaxis={
+                'showgrid': False,
+                'autorange': True,
+                'zeroline': False,
+                'showline': False,
+                'showticklabels': False
+
+            }
+        )
+    }
+
+
+@app.callback(
+    Output('val_Infectados', 'children'),  [Input('comunidad-drop', 'value')]
+)
+def value_infectados(comunidad):
+    return dataset.df[dataset.df['CCAA'] == comunidad]['Casos'].iloc[-1]
+
+
+@app.callback(
+    Output('val_Fallecidos', 'children'),  [Input('comunidad-drop', 'value')]
+)
+def value_fallecidos(comunidad):
+    return dataset.df[dataset.df['CCAA'] == comunidad]['Fallecidos'].iloc[-1]
+
+
+@app.callback(
+    Output('val_Recuperados', 'children'),  [Input('comunidad-drop', 'value')]
+)
+def value_recuperados(comunidad):
+    return dataset.df[dataset.df['CCAA'] == comunidad]['Recuperados'].iloc[-1]
+
+
+@app.callback(
+    Output('val_Casos activos', 'children'),  [
+        Input('comunidad-drop', 'value')]
+)
+def value_activos(comunidad):
+    casos = dataset.df[dataset.df['CCAA'] == comunidad]['Casos'].iloc[-1]
+    fallecidos = dataset.df[dataset.df['CCAA']
+                            == comunidad]['Fallecidos'].iloc[-1]
+    recuperados = dataset.df[dataset.df['CCAA']
+                             == comunidad]['Recuperados'].iloc[-1]
+
+    return casos - fallecidos - recuperados
+
+
+@app.callback(
+    Output('inc_Infectados', 'children'),  [Input('comunidad-drop', 'value')]
+)
+def inc_infectados(comunidad):
+    return "🠝 " + str(dataset.df[dataset.df['CCAA'] == comunidad]['Casos'].iloc[-1] - dataset.df[dataset.df['CCAA'] == comunidad]['Casos'].iloc[-2]) + " (24h)"
+
+
+@app.callback(
+    Output('inc_Fallecidos', 'children'),  [Input('comunidad-drop', 'value')]
+)
+def inc_fallecidos(comunidad):
+    return "🠝 " + str(dataset.df[dataset.df['CCAA'] == comunidad]['Fallecidos'].iloc[-1] - dataset.df[dataset.df['CCAA'] == comunidad]['Fallecidos'].iloc[-2]) + " (24h)"
+
+
+@app.callback(
+    Output('inc_Recuperados', 'children'),  [Input('comunidad-drop', 'value')]
+)
+def inc_recuperados(comunidad):
+    return "🠝 " + str(dataset.df[dataset.df['CCAA'] == comunidad]['Recuperados'].iloc[-1] - dataset.df[dataset.df['CCAA'] == comunidad]['Recuperados'].iloc[-2]) + " (24h)"
